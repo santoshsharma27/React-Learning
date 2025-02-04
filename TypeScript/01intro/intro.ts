edf2772d18291eb9d41237335364ecc736da2e8d
santoshsharma27 <santosh37kr@gmail.com>
2025-02-04 23:25:01 +0530

    Leaning TypeScript Day1

---
+++ TypeScript/01intro/intro.ts
@@ -0,0 +1,7 @@
+let user = {
+  name: "Santosh",
+  age: 30,
+};
+
+console.log("Santosh");
+console.log(user.name);
---
+++ TypeScript/01intro/variableMe.ts
@@ -0,0 +1,24 @@
+let greetings: string = "Hello Santosh";
+greetings.toLowerCase();
+console.log(greetings);
+
+// number
+let num = 10;
+num.toFixed();
+console.log(num);
+
+// boolean
+let isStudent: boolean = true;
+console.log(isStudent);
+
+//any
+
+let hero: string;
+
+function getHero() {
+  return "Thor";
+}
+
+hero = getHero();
+
+export {};
