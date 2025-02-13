c824341b514633fe58b75676e3246f177200ea79
santoshsharma27 <santosh37kr@gmail.com>
2025-02-13 10:03:35 +0530

    Leaning TypeScript Day4

--- TypeScript/02basics/myArray.ts
+++ TypeScript/02basics/myArray.ts
@@ -1,6 +1,6 @@
 const superHeros: string[] = [];
-// const heroPower: number[] = []
-const heroPower: Array<number> = [];
+const heroPower: number[] = [];
+const heroPowers: Array<number> = [];
 
 type User = {
   name: string;
