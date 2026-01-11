1f54d8c71e99fcd52e4f6c4a1b0a0eb104d4bc4a
santoshsharma27 <santosh37kr@gmail.com>
2026-01-11 21:18:46 +0530

    Fixed pagination

--- LLD/src/components/pagination/Pagination.jsx
+++ LLD/src/components/pagination/Pagination.jsx
@@ -18,11 +18,12 @@ function Pagination() {
     setIsLoading(true);
     setError(null);
     try {
-      const res = await fetch("https://dummyjson.com/products?limit=200");
+      const res = await fetch("https://dummyjson.com/products?limit=100");
       if (!res.ok)
         throw new Error("Something went wrong while fetching products");
 
       const data = await res.json();
+      console.log(data);
       setProducts(data.products);
     } catch (err) {
       setError(err.message);
@@ -43,62 +44,69 @@ function Pagination() {
     if (currentPage > 0) setCurrentPage((prev) => prev - 1);
   }
 
+  if (isLoading) {
+    return (
+      <h2 className="flex h-screen items-center justify-center text-xl">
+        Loading...
+      </h2>
+    );
+  }
+
+  if (error) {
+    <p className="text-center text-lg font-medium text-red-500">{error}</p>;
+  }
+
+  if (!isLoading && products.length === 0) {
+    return (
+      <p className="flex h-screen items-center justify-center text-xl">
+        No products available
+      </p>
+    );
+  }
+
   return (
     <div className="min-h-screen">
       <h1 className="mb-6 text-center text-2xl font-extrabold">Pagination</h1>
-
-      {isLoading ? (
-        <h2 className="flex h-screen items-center justify-center text-xl">
-          Loading...
-        </h2>
-      ) : error ? (
-        <p className="text-center text-lg font-medium text-red-500">{error}</p>
-      ) : products.length === 0 ? (
-        <p className="text-center text-lg font-medium text-gray-700">
-          No products available
-        </p>
-      ) : (
-        <div>
-          <div className="mb-8 flex flex-wrap justify-center gap-8">
-            {products.slice(start, end).map((product) => (
-              <ProductCard key={product.id} {...product} />
-            ))}
-          </div>
-
-          {/* Pagination Controls */}
-          <div className="fixed right-0 bottom-0 left-0 flex items-center justify-center gap-4 bg-white py-4 shadow-md">
-            <button
-              className="rounded-md bg-blue-600 px-4 text-lg text-white shadow-md transition duration-300 hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-300"
-              onClick={handlePrev}
-              disabled={currentPage === 0}
-            >
-              {"<"}
-            </button>
-
-            {Array.from({ length: noOfPages }).map((_, index) => (
-              <button
-                className={`rounded-md px-2 text-lg font-semibold transition duration-300 ${
-                  index === currentPage
-                    ? "bg-blue-600 font-bold text-white"
-                    : "text-gray-700 hover:bg-blue-200"
-                }`}
-                key={index}
-                onClick={() => setCurrentPage(index)}
-              >
-                {index + 1}
-              </button>
-            ))}
-
-            <button
-              className="rounded-md bg-blue-600 px-4 text-lg text-white shadow-md transition duration-300 hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-300"
-              onClick={handleNext}
-              disabled={currentPage === noOfPages - 1}
-            >
-              {">"}
-            </button>
-          </div>
+      <div>
+        <div className="mb-8 flex flex-wrap justify-center gap-8">
+          {products.slice(start, end).map((product) => (
+            <ProductCard key={product.id} {...product} />
+          ))}
         </div>
-      )}
+
+        {/* Pagination Controls */}
+        <div className="fixed right-0 bottom-0 left-0 flex items-center justify-center gap-4 bg-white py-4 shadow-md">
+          <button
+            className="rounded-md bg-blue-600 px-4 text-lg text-white shadow-md transition duration-300 hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-300"
+            onClick={handlePrev}
+            disabled={currentPage === 0}
+          >
+            {"<"}
+          </button>
+
+          {Array.from({ length: noOfPages }).map((_, index) => (
+            <button
+              className={`rounded-md px-2 text-lg font-semibold transition duration-300 ${
+                index === currentPage
+                  ? "bg-blue-600 font-bold text-white"
+                  : "text-gray-700 hover:bg-blue-200"
+              }`}
+              key={index}
+              onClick={() => setCurrentPage(index)}
+            >
+              {index + 1}
+            </button>
+          ))}
+
+          <button
+            className="rounded-md bg-blue-600 px-4 text-lg text-white shadow-md transition duration-300 hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-300"
+            onClick={handleNext}
+            disabled={currentPage === noOfPages - 1}
+          >
+            {">"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
